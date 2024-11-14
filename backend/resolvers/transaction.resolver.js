6c0505f1bab8aefae427796758e0431f1bcb7037
kabir4874 <kabir.cse.bd@gmail.com>
2024-11-15 00:36:24 +0600

    transaction resolver

--- backend/resolvers/transaction.resolver.js
+++ backend/resolvers/transaction.resolver.js
@@ -1,5 +1,29 @@
+import Transaction from "../models/transaction.model.js";
+
 const transactionResolver = {
-  Query: {},
+  Query: {
+    transactions: async (_, __, context) => {
+      try {
+        if (!context.getUser()) throw new Error("Unauthorized");
+        const userId = await context.getUser()._id;
+        const transactions = await Transaction.find({ userId });
+        return transactions;
+      } catch (error) {
+        console.error("Error in transactions query", error);
+        throw new Error(error.message || "Internal server error");
+      }
+    },
+
+    transaction: async (_, { transactionId }) => {
+      try {
+        const transaction = await Transaction.findById(transactionId);
+        return transaction;
+      } catch (error) {
+        console.error("Error in transaction query", error);
+        throw new Error(error.message || "Internal server error");
+      }
+    },
+  },
   Mutation: {},
 };
 export default transactionResolver;

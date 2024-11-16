81c06285571e89ce0f0dff9392b01fa628e38bef
kabir4874 <kabir.cse.bd@gmail.com>
2024-11-16 21:57:28 +0600

    frontend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,25 @@
-
+import { Route, Routes } from "react-router-dom";
+import Header from "./components/ui/Header";
+import HomePage from "./pages/HomePage";
+import LoginPage from "./pages/LoginPage";
+import NotFoundPage from "./pages/NotFoundPage";
+import SignUpPage from "./pages/SignUpPage";
+import TransactionPage from "./pages/TransactionPage";
 
 const App = () => {
+  const authUser = true;
   return (
-    <div>App</div>
-  )
-}
+    <>
+      {authUser && <Header />}
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/signup" element={<SignUpPage />} />
+        <Route path="/transaction/:id" element={<TransactionPage />} />
+        <Route path="*" element={<NotFoundPage />} />
+      </Routes>
+    </>
+  );
+};
 
-export default App
+export default App;
